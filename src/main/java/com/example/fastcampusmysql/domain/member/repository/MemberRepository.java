a71d138b2724dc3140366eadeca275fd11f10c54
Jay Lee <codebyjaylee@gmail.com>
2023-09-08 00:17:06 +0900

    회원 이름 변경 내역 저장 구현

--- src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.example.fastcampusmysql.domain.member.repository;
 
 import com.example.fastcampusmysql.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -13,14 +14,24 @@ import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
 public class MemberRepository {
-    final private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    private static final String TABLE = "member";
+    private static final String TABLE = "Member";
+    static final RowMapper<Member> rowMapper = (ResultSet resultSet, int rowNum) -> Member
+            .builder()
+            .id(resultSet.getLong("id"))
+            .email(resultSet.getString("email"))
+            .nickname(resultSet.getString("nickname"))
+            .birthday(resultSet.getObject("birthday", LocalDate.class))
+            .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
+            .build();
+
     public Optional<Member> findById(Long id){
         /*
             select *
@@ -28,18 +39,12 @@ public class MemberRepository {
             where id = id
         */
         var sql = String.format("SELECT * FROM %s WHERE id = :id", TABLE);
-        var param = new MapSqlParameterSource()
+        var params = new MapSqlParameterSource()
                 .addValue("id", id);
-        RowMapper<Member> rowMapper = (ResultSet resultSet, int rowNum) -> Member
-                .builder()
-                .id(resultSet.getLong("id"))
-                .email(resultSet.getString("email"))
-                .nickname(resultSet.getString("nickname"))
-                .birthday(resultSet.getObject("birthday", LocalDate.class))
-                .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
-                .build();
-        var member = namedParameterJdbcTemplate.queryForObject(sql, param, rowMapper);
-        return Optional.ofNullable(member);
+        List<Member> members = namedParameterJdbcTemplate.query(sql, params, rowMapper);
+
+        Member nullableMember = DataAccessUtils.singleResult(members);
+        return Optional.ofNullable(nullableMember);
     }
     public Member save(Member member){
         /*
@@ -70,7 +75,9 @@ public class MemberRepository {
     }
 
     private Member update(Member member){
-        // TODO: implemented
+        var sql = String.format("UPDATE %s set email = :email, nickname = :nickname, birthday = :birthday WHERE id = :id", TABLE);
+        SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+        namedParameterJdbcTemplate.update(sql, params);
         return member;
     }
 }
